6f6095837a32db0b7361c86a582335a4ccee26f0
kennyboy106 <tang.kenneth720@gmail.com>
2024-09-04 12:24:08 -0700

    R1 of plan created

---
+++ main.py
@@ -0,0 +1,29 @@
+
+
+
+
+if __name__ == "__main__":
+    # Initialize a database file
+    # Initialize a table for raw transactions
+    '''
+    Initialize a table for processed transactions
+        Coloumns shall consist of:
+        Ticker
+        Date of purchase
+        Quantity Buy
+        Cost basis Buy (cost of the transaction)
+        Date of sale
+        Quantity Sell
+        Cost basis Sell (cost of the transaction)
+
+    '''     
+
+
+    # Import data from CSVs, for now, just from chase or fidelity
+    # Start processing the data and inserting it into te processed transactions table
+    # If there is a transaction that went CIL, just say that we sold all the shares we had and use the CIL amount as the cost basis sell
+    # Calculate capital gains from the processed transcations table
+
+    pass
+
+
