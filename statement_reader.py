b740736492a0822f82e69366609f0b8d8a23cc77
Kenny <tang.kenneth720@gmail.com>
2024-09-13 22:04:53 -0700

    Collected main information from multi statement

--- statement_reader.py
+++ statement_reader.py
@@ -7,7 +7,7 @@ smaller functions should be like:
     Get account value
     Get xxx
 Large function should return a dictionary of all values that might be needed
-Maybe the larger functions can just return certain values that are determined by key word agruments passed in.
+Maybe the larger functions can just return certain values that are determined by key word arguments passed in.
     If n keywords are passed in then n things are returned from the document
 '''
 
@@ -47,15 +47,19 @@ for page in doc:
     # Get period value
     # Get current value
     # Get capital gains
+    
+    # Get all page text
     ret = page.get_text('words', sort=True)
 
-    prev_period_value = 0
-    cur_period_value = 0
+    prev_period_value = None
+    cur_period_value = None
     acc_number = None
-    short_term_net_gains = 0
-    long_term_net_gains = 0
+    short_term_net_gains = None
+    short_term_net_gains_YTD = None
+    long_term_net_gains = None
+    long_term_net_gains_YTD = None
     
-    
+    page_flag = False
     
     for i, word in enumerate(ret):
         text = word[4]
@@ -66,36 +70,75 @@ for page in doc:
         1. Get the account number on the page
         2. Try getting all information if it hasn't already been acquired with that account number
         '''
-        
+        # Ensure this page has relevant info 
         if 'Acct' in text:
             try:
                 acc_number = ret[i + 2][4]  # Get the account number which should be after the following # sign
                 acc_number = acc_number.replace(')', '')    # Remove the ) that follows
                 acc_number = acc_number[-4:]    # Keep only the last 4 numbers of the account
                 if int(acc_number):
-                    pass
-                # TODO I need to put a flag here that indicates that this page is good to start extracting info from
-            # Do stuff for this account page
+                    page_flag = True
             
             except:
                 pass
         
-        # Nothing on this page? Go to the next page
-        else:
-            break
-        
+        # Nothing on this page pertaining to this account? Go to the next page
+        # But if we did get the account number earlier, don't skip the page
+        if page_flag:
+            
+            # Extract the statement ending date #
+            # If we haven't gotten the year yet
+            if not year and text == 'Statement':
+                try:
+                    if ret[i + 1][4] == 'Period:':
+                        month = ret[i + 2][4].lower()
+                        day = ret[i + 6][4][:-1]
+                        year = ret[i + 7][4]
+                        if Months[month] and int(day) and int(year):
+                            print('Date acquired')
+                except:
+                    # Reset if we had any errors
+                    month, day, year = (None,) * 3
+            
+            # Extract the previous period value and current value
+            if not cur_period_value and text == 'TOTAL':
+                try:
+                    if ret[i + 1][4] == 'ACCOUNT' and ret[i + 2][4] == 'VALUE':
+                        prev_period_value = ret[i + 3][4].replace('$', '')
+                        cur_period_value = ret[i + 4][4].replace('$', '')
+                        prev_period_value = float(prev_period_value)
+                        cur_period_value = float(cur_period_value)
+                except:
+                    cur_period_value = prev_period_value = None
+            
+            # Extract the short term gains
+            if not short_term_net_gains and text == 'Short-Term':
+                try:
+                    if ret[i + 1][4] == 'Net' and ret[i + 2][4] == 'Gain':
+                        short_term_net_gains = ret[i + 5][4].replace('$', '')
+                        short_term_net_gains_YTD = ret[i + 6][4].replace('$', '')
+                        short_term_net_gains = float(short_term_net_gains)
+                        short_term_net_gains_YTD = float(short_term_net_gains_YTD)
+                except:
+                    short_term_net_gains = short_term_net_gains_YTD = None
+            
+            # Extract the long term gains
+            if not long_term_net_gains and text == 'Long-Term':
+                try:
+                    if ret[i + 1][4] == 'Net' and ret[i + 2][4] == 'Gain':
+                        long_term_net_gains = ret[i + 5][4].replace('$', '')
+                        long_term_net_gains_YTD = ret[i + 6][4].replace('$', '')
+                        long_term_net_gains = float(long_term_net_gains)
+                        long_term_net_gains_YTD = float(long_term_net_gains_YTD)
+                except:
+                    long_term_net_gains = long_term_net_gains_YTD = None
 
-
-
-# -------------------- # Extract the statement ending date # ---------------- #
-# if not year and text == 'Statement':
-#     try:
-#         if ret[i + 1][4] == 'Period:':
-#             month = ret[i + 2][4].lower()
-#             day = ret[i + 6][4][:-1]
-#             year = ret[i + 7][4]
-#             if Months[month] and int(day) and int(year):
-#                 print('Date acquired')
-#     except:
-#         month, day, year = (None,) * 3
-        
+    # Check and save for any info collected with account number
+    if page_flag and (type(prev_period_value) == float or short_term_net_gains):
+        print(f'Information found')
+        print(f'Account number: {acc_number}')
+        print(f'Statement date: {Months[month].value}/{int(day)}/{int(year)}')
+        print(f'Previous value: {prev_period_value}')
+        print(f'Current value: {cur_period_value}')
+        print(f'Short-term net gains: {short_term_net_gains} {short_term_net_gains_YTD}')
+        print(f'Long-term net gains: {long_term_net_gains} {long_term_net_gains_YTD}')
