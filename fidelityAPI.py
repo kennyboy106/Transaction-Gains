06ef62cae3bd88ce3e28c0825d464fbb751546f2
Kenny <tang.kenneth720@gmail.com>
2024-09-18 19:23:41 -0700

    Transaction function working!!

--- fidelityAPI.py
+++ fidelityAPI.py
@@ -94,81 +94,111 @@ class FidelityAutomation:
             traceback.print_exc() 
             return False
 
-    def fidelitytransaction(self, stock: str, quantity: float, action: str, account: str) -> bool:
-        # Go to the trade page
-        if self.page.url != 'https://digital.fidelity.com/ftgw/digital/trade-equity/index/orderEntry':
-            self.page.goto('https://digital.fidelity.com/ftgw/digital/trade-equity/index/orderEntry')
+    def fidelitytransaction(self, stock: str, quantity: float, action: str, account: str, dry: bool=True) -> bool:
+        '''
+        TODO after return on error, trim the error message down a lot. There is a lot of whitespace. Its all whitespace
         
-        # Ensure we are in the simplified ticket
-        if self.page.get_by_role("button", name="View simplified ticket").is_visible():
-            self.page.get_by_role("button", name="View simplified ticket").click()
-            # Wait for it to take effect
-            self.page.get_by_text("Buy", exact=True).wait_for(timeout=2000)
-        
-        # Click on the drop down
-        self.page.query_selector("#dest-acct-dropdown").click()
-        # Find the account to trade under
-        self.page.get_by_role("option").filter(has_text=account).click()
-
-        # Enter the symbol
-        self.page.get_by_label("Symbol").click()
-        # Fill in the ticker
-        self.page.get_by_label("Symbol").fill(stock)
-        # Find the symbol we wanted and click it
-        self.page.get_by_text(stock.upper(), exact=True).click()
-
-        # Wait for quote panel to show up
-        self.page.locator("#quote-panel").wait_for(timeout=2000)
-        last_price = self.page.query_selector("#eq-ticket__last-price > span.last-price").text_content()
-        last_price = last_price.replace('$','')
-        # Enable extended hours trading if available
-        if self.page.locator(".eq-ticket_extendedhour_toggle-item").is_visible():
-            self.page.locator(".eq-ticket_extendedhour_toggle-item").check()
-        
-        # Press the buy or sell button. Title capitalizes the first letter so 'buy' -> 'Buy'
-        self.page.locator("label").filter(has_text=action.title()).click()
-        # Press the shares text box
-        self.page.locator("label").filter(has_text="Shares").click()
-        self.page.get_by_text("Share amount").click()
-        self.page.get_by_label("Share amount").fill(str(quantity))
-        # If it should be limit
-        if float(last_price) < 1 and action.lower() == 'buy':
-            difference_price = 0.01 if float(last_price) > 0.1 else 0.0001
-            wanted_price = round(float(last_price) + difference_price, 3)
-            # Click on the limit
-            self.page.locator("label").filter(has_text="Limit").click()
-            # Enter the limit price
-            self.page.get_by_text("Limit price").click()
-            self.page.get_by_label("Limit price").fill(str(wanted_price))
-        # Otherwise its market
-        else:
-            # Click on the limit
-            self.page.locator("label").filter(has_text="Market").click()
-
-        # Ensure its a day trade
-        self.page.get_by_text("Day", exact=True).click()
-        # Continue with the order
-        self.page.get_by_role("button", name="Preview order").click()
-
-        # If error occurred
+        '''
         try:
-            self.page.get_by_role("button", name="Place order clicking this").wait_for(timeout=1500)
+            # Go to the trade page
+            if self.page.url != 'https://digital.fidelity.com/ftgw/digital/trade-equity/index/orderEntry':
+                self.page.goto('https://digital.fidelity.com/ftgw/digital/trade-equity/index/orderEntry')
+            
+            # Ensure we are in the simplified ticket
+            if self.page.get_by_role("button", name="View simplified ticket").is_visible():
+                self.page.get_by_role("button", name="View simplified ticket").click()
+                # Wait for it to take effect
+                self.page.get_by_text("Buy", exact=True).wait_for(timeout=2000)
+            
+            # Click on the drop down
+            self.page.query_selector("#dest-acct-dropdown").click()
+            # Find the account to trade under
+            self.page.get_by_role("option").filter(has_text=account).click()
+
+            # Enter the symbol
+            self.page.get_by_label("Symbol").click()
+            # Fill in the ticker
+            self.page.get_by_label("Symbol").fill(stock)
+            # Find the symbol we wanted and click it
+            self.page.get_by_text(stock.upper(), exact=True).click()
+
+            # Wait for quote panel to show up
+            self.page.locator("#quote-panel").wait_for(timeout=2000)
+            last_price = self.page.query_selector("#eq-ticket__last-price > span.last-price").text_content()
+            last_price = last_price.replace('$','')
+            # Enable extended hours trading if available
+            if self.page.locator(".eq-ticket_extendedhour_toggle-item").is_visible():
+                self.page.locator(".eq-ticket_extendedhour_toggle-item").check()
+            
+            # Press the buy or sell button. Title capitalizes the first letter so 'buy' -> 'Buy'
+            self.page.locator("label").filter(has_text=action.title()).click()
+            # Press the shares text box
+            self.page.locator("label").filter(has_text="Shares").click()
+            self.page.get_by_text("Share amount").click()
+            self.page.get_by_label("Share amount").fill(str(quantity))
+            # If it should be limit
+            if float(last_price) < 1 and action.lower() == 'buy':
+                difference_price = 0.01 if float(last_price) > 0.1 else 0.0001
+                wanted_price = round(float(last_price) + difference_price, 3)
+                # Click on the limit
+                self.page.locator("label").filter(has_text="Limit").click()
+                # Enter the limit price
+                self.page.get_by_text("Limit price").click()
+                self.page.get_by_label("Limit price").fill(str(wanted_price))
+            # Otherwise its market
+            else:
+                # Click on the limit
+                self.page.locator("label").filter(has_text="Market").click()
+
+            # Ensure its a day trade
+            if self.page.get_by_text("Day", exact=True).is_visible():
+                self.page.get_by_text("Day", exact=True).click()
+            # Continue with the order
+            self.page.get_by_role("button", name="Preview order").click()
+
+            # If error occurred
+            try:
+                self.page.get_by_role("button", name="Place order clicking this").wait_for(timeout=1500, state='visible')
+            except PlaywrightTimeoutError:
+                # Error must be present (or really slow page for some reason)
+                # Try to report on error
+                error_message = None
+                try:
+                    error_message = self.page.get_by_label("Error").locator("div").filter(has_text="critical").nth(2).text_content()            
+                    self.page.get_by_role("button", name="Close dialog").click()
+                    
+                except:
+                    pass
+                # Return with error
+                print("Error occurred")
+                return (False, error_message)
+            
+            # If no error occurred, continue with checking and buy/sell
+            try:
+                assert self.page.locator("preview").filter(has_text=account).is_visible()
+                assert self.page.get_by_text(f"Symbol{stock.upper()}", exact=True).is_visible()
+                assert self.page.get_by_text(f"Action{action.title()}").is_visible()
+                assert self.page.get_by_text(f"Quantity{quantity}").is_visible()
+            except AssertionError:
+                return (False, 'Order preview is not what is expected')
+            
+            # If its a real run
+            if not dry:
+                self.page.get_by_role("button", name="Place order clicking this").click()
+                try:
+                    # See that the order goes through
+                    self.page.get_by_text("Order received").wait_for(timeout=5000, state='visible')
+                    # If no error, return with success
+                    return (True, None)
+                except PlaywrightTimeoutError:
+                    # Order didn't go through for some reason, go to the next and say error
+                    return (False, 'Order failed to complete')
+            # If its a dry run, report back success
+            return (True, None)
         except PlaywrightTimeoutError:
-            # Error must be present (or really slow page for some reason)
-            # Try to report on error
-            error_message = self.page.get_by_label("Error").locator("div").filter(has_text="critical").text_content()
-            print(error_message)
-            self.page.get_by_role("button", name="Close dialog").click()
-        
-        # If no error occurred, continue with checking and buy
-        try:
-            assert self.page.locator("preview").filter(has_text=account).is_visible()
-            assert self.page.get_by_text(f"Symbol{stock.upper()}", exact=True).is_visible()
-            assert self.page.get_by_text(f"Action{action.title()}").is_visible()
-            assert self.page.get_by_text(f"Quantity{'%.2f' % wanted_price}").is_visible()
-        except AssertionError:
-            print(f'Order preview is not what is expected')
-
+            return (False, 'Driver timed out. Order not complete')
+        except Exception as e:
+            return (False, e)
 
     
     def getAccountInfo(self):
@@ -213,7 +243,9 @@ try:
         acc = acc.split(':')
         fid.fidelitylogin(acc[0], acc[1])
         # Individual 12 (Z32228331)
-    fid.fidelitytransaction('aabb', 1, 'buy', 'Z32228331')
+    success, message = fid.fidelitytransaction('intc', 1, 'sell', 'Z09449756', False)
+    if not success:
+        print(message)
     # fid.getAccountInfo()
 except Exception as e:
     print(e)
